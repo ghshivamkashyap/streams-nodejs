5fffa88eff05386e76f5b247af81ad4d840c8b6a
SHIVAM KASHYAP <lootforecast@gmail.com>
2025-06-22 22:28:44 +0530

    file transfer wrong way - single piece

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const hostname = "127.0.0.1";
 const port = 4000;
 
+/// file transfer single piece
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   //   res.setHeader("Content-Type", "text/plain");
